f7f95cfb4b6189953453e8b8c40088fc7130f30a
Mikey Kriegel <mikkrieg@gmail.com>
2021-04-02 14:27:30 -0700

    Finish CRUD functionality of AnimalsController

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -99,5 +99,19 @@ namespace AnimalShelter.Controllers
     {
       return _db.Animals.Any(e => e.AnimalId == id);
     }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
   }
 }
