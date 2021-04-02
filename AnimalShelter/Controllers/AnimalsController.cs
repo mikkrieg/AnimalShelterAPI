3bfc0107e7d4b26e9b00509b53c5760ab0b31b7c
Mikey Kriegel <mikkrieg@gmail.com>
2021-04-02 13:36:24 -0700

    Update get route to take queries, add more routes, still WIP

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -1,10 +1,11 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using TodoApp.Data;
-using TodoApp.Models;
+using AnimalShelter.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AnimalShelter.Controllers
 {
@@ -21,27 +22,82 @@ namespace AnimalShelter.Controllers
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAnimals()
+    public async Task<ActionResult<IEnumerable<Animal>>>Get(string name, string gender, string species, string personality )
     {
-      var animals = await _db.Animals.ToListAsync();
-      return Ok(animals);
+      var query = _db.Animals.AsQueryable();
+      if(name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+      if(gender != null)
+      {
+        query = query.Where(entry => entry.Gender == gender);
+      }
+      if(species != null)
+      {
+        query = query.Where(entry => entry.Species == species);
+      }
+      if(personality != null)
+      {
+        query = query.Where(entry => entry.Personality == personality);
+      }
+      return await query.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+
+      if(animal == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(animal);
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateAnimal(Animal animal)
     {
-      if(ModelState.IsValid)
-      {
-        await _db.Animals.AddAsync(animal);
-        await _db.SaveChangesAsync();
+      
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
 
-        return CreatedAtAction("GetAnimal", new {animal.AnimalId}, animal);
-      }
-
-      return new jsonResult("Something went wrong") {StatusCode = 500};
+      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId}, animal);
     }
 
-    
-  }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateItem(int id, Animal animal)
+    {
+      if(id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
 
+      _db.Entry(animal).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
+    }
+  }
 }
